90f5ab06e647e8ffbf9dabc69fe7b272307cb0e5
wdong5 <38866689+wdong5@users.noreply.github.com>
2020-06-20 12:30:26 -0700

    Update llvm_compile.py

--- example/triad/llvm_compile.py
+++ example/triad/llvm_compile.py
@@ -23,16 +23,23 @@ def main (directory, source):
   LLVM_HOME = os.getenv('TRACER_HOME')
 
   source_file = source + '.c'
+  print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
   os.system('%s/bin/get-labeled-stmts triad.c -- -I%s/lib/clang/3.4' % (TRACER_HOME, LLVM_HOME))
+  print("BAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
   os.system('clang -static -g -O1 -S -fno-slp-vectorize -fno-vectorize ' + \
             ' -fno-unroll-loops -fno-inline -fno-builtin -emit-llvm -o ' + \
             obj + ' '  + source_file)
+  print("CAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
   os.system('opt -disable-inlining -S -load=' + TRACER_HOME + \
             '/lib/full_trace.so -fulltrace -labelmapwriter ' + obj + ' -o ' + opt_obj)
+  print("DAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
   os.system('llvm-link -o full.llvm ' + opt_obj + ' ' + \
             TRACER_HOME + '/lib/trace_logger.llvm')
+  print("EAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
   os.system('llc -O0 -disable-fp-elim -filetype=asm -o full.s full.llvm')
-  os.system('gcc -static -O0 -fno-inline -o ' + executable + ' full.s -lm -lz')
+  print("FAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
+  os.system('gcc -static -O0 -fno-inline -o ' + executable + ' full.s -lm -lz -lpthread -lstdc++')
+  print("GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
   os.system('./' + executable)
 
 if __name__ == '__main__':
